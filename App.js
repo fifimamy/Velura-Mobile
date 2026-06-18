224ccbe7506b4985c9af16940d8896088dde628e
fifimamy <fifimamy18@gmail.com>
2026-06-18 18:06:11 +0100

    fix expo doctor

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ export default function App() {
     <NavigationContainer>
      <Stack.Navigator 
      screenOptions={{ headerShown: false }}
-     initialRouteName="SIGN_IN">
+     initialRouteName={SCREENS.SPLASH}>
 
        <Stack.Screen name={SCREENS.SPLASH} component={SplashScreen} />
        <Stack.Screen name={SCREENS.SIGN_IN} component={SignInScreen} />
